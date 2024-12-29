b823f980a296b2a1e91b321ca3fa0fb577f91b80
Omar <osalhe12@gmail.com>
2024-12-28 23:45:45 -0500

    learning bash

--- process_leets.sh
+++ process_leets.sh
@@ -1,13 +1,23 @@
 #! /usr/bin/env bash
 
-FILE = "leets.txt"
+input_file = "unprocessed.txt
+output_file = "processed.txt"
 
-if [[ ! -f "$FILE"]]; then
-	echo "File not found !!!"
+if [[ ! -f "$input_file"]]; then
+	echo "No Leetcodes to process or incorrect file provided:
 	exit 1
 fi
 
+
+i = 0
 while IFS= -r read line
 do
+	if [$line = 1]; then
+		name = $line
+	if [ $line = 2 ]; then
+		awk 
+	if [ $line =  3 ]; then
 	
-done
+	echo $line >> output_file
+	((i++))	
+done < input_file
