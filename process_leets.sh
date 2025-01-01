4f0f6116e14f2fbcbcd1c5a04ee386f1b62a0d18
Omar <osalhe12@gmail.com>
2024-12-31 22:53:45 -0500

    finished file reading + adding input validation + added error logging

--- process_leets.sh
+++ process_leets.sh
@@ -1,23 +1,51 @@
 #! /usr/bin/env bash
 
-input_file = "unprocessed.txt
-output_file = "processed.txt"
+input_file="unprocessed.txt"
+output_file="processed.txt"
 
-if [[ ! -f "$input_file"]]; then
-	echo "No Leetcodes to process or incorrect file provided:
+solution_file="dummy.txt"
+
+if [[ ! -f "$input_file" ]]; then
+	echo "Cannot access solutions to process."
 	exit 1
 fi
 
-
-i = 0
+difficulty=""
+name=""
+type=""
+ 
 while IFS= -r read line
-do
-	if [$line = 1]; then
-		name = $line
-	if [ $line = 2 ]; then
-		awk 
-	if [ $line =  3 ]; then
-	
-	echo $line >> output_file
-	((i++))	
-done < input_file
+do	
+	if [[ -z "$line" || "$line" =~ ^[[:space:]]*$ ]]; then
+        	echo "Warning: Skipping empty line."
+        	continue
+    	fi
+ 
+	if [[ "$line" == "EOS" ]]; then
+		difficulty=""
+		name=""
+		type=""
+		break
+	fi
+
+	if [[ "$solution_file" == "dummy.txt" ]]; then
+		IFS=',' read difficulty name type <<< "$line"
+		if [[ -z "$difficulty" || -z "$name" || -z "$type" ]]; then
+    			echo "Error: Malformed input line - $line"
+   	 		continue
+		fi
+ 
+		mkdir -p "$difficulty/$name"
+		solution_file="$difficulty/$name/$name.$type"
+		touch "$solution_file"
+
+		if [[ ! -f "$solution_file" ]]; then
+			echo "Failed to create solution file"
+			exit 1
+		fi
+	else
+		echo $line >> "$solution_file"
+	fi
+
+	echo $line >> output_file	
+done < input_file 
